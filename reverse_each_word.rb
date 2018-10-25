6d9da46913cc2a0e36cc98328b16c3573f828b9d
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 12:28:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   reversed = sentence1.split
-  reversed.collect do |sentence1|
-    sentence1.reverse!
+  reversed.collect do |word|
+    word.reverse!
   end
   reversed.join(" ")
 end
